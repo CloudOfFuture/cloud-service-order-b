8359e9716a0aa025f83c59b2083aada18ff4b684
Jarven <769558579@qq.com>
2017-12-25 18:44:00 +0800

    测试地址

--- src/main/java/com/kunlun/api/controller/IndexController.java
+++ src/main/java/com/kunlun/api/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.kunlun.api.controller;
 import com.kunlun.result.DataRet;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,8 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 
-    @GetMapping("/test")
-    public DataRet<String> test(){
-        return new DataRet<>("这是服务B");
+    /**
+     * 测试服务之间调用
+     *
+     * @return
+     */
+    @GetMapping("/log")
+    public DataRet<String> logTest(@RequestParam("orderNo") String orderNo) {
+        return new DataRet<>("我是服务B：orderNo=" + orderNo);
     }
 }
